34c20a65674482f6b96a2a18d0109261cdb11a34
JhairAgila <jhairagil@gmail.com>
2024-07-20 10:43:41 -0500

    feature/CRUD activities system

--- system-activities-module/src/controllers/systemActivityController.js
+++ system-activities-module/src/controllers/systemActivityController.js
@@ -1,8 +1,61 @@
-const getAllSystemActivities = () => {};
+const SystemActivity = require("../models/SystemActivity");
 
-const getSystemActivityById = () => {};
 
-const createSystemActivity = async () => {};
+const getAllSystemActivities = (req, res, next) => {
+  try {
+    return res.status(200).json({
+      customMessage: "Mensaje de prueba";
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getSystemActivityById = (req, res, next) => {
+  try {
+    return res.status(200).json({
+      customMessage: "Mensaje de prueba",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const createSystemActivity = async (req, res, next) => {
+  const { model, route, ...body } = req.body;
+
+  try {
+    const systemActivityAlreadyExists = await SystemActivity.findOne({
+      model, 
+      deleteAt: null,
+    });
+
+    if(systemActivityAlreadyExists){
+      return next({
+        status: 400,
+        customMessage: "Actividad del sistema ya registrada"
+      });
+    }
+
+    const systemActivity = await SystemActivity.create({
+      model,
+      route,
+      ...body,
+    });
+
+    return res.status(201).json({
+      customMessage: "Actividad del sistema registrada exitosamente",
+      results: systemActivity,
+    });
+
+  } catch (error) {
+    return next({
+      status: 500,
+      customMessage: "Error al guardar la actividad del sistema",
+      error: error.message
+    })    
+  }
+};
 
 module.exports = {
   getAllSystemActivities,
