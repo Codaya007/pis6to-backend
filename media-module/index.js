aa201fdf72bce39fd851ab2c7981d298757f0d78
vivi <viviana.calva@lumation.co>
2024-07-29 14:05:21 -0500

    Adding loger

--- media-module/index.js
+++ media-module/index.js
@@ -7,10 +7,12 @@ const { PORT } = process.env;
 const errorHandler = require("./middlewares/errorHandler.js");
 const notFound = require("./middlewares/notFound.js");
 const mediaRouter = require("./routes/media.routes.js");
+const logger = require("morgan");
 
 const app = express();
 
 app.use(cors());
+app.use(logger("dev"));
 
 // Middleware para analizar solicitudes con cuerpo JSON
 app.use(express.json());
